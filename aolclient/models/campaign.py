c8757377b40845d189786a16aad0d97cb4af4744
vagrant <vagrant@vagrant.vm>
2016-05-26 18:22:33 +0000

    add v2 to campaign url in Campaign class for aol

--- aolclient/models/campaign.py
+++ aolclient/models/campaign.py
@@ -9,17 +9,17 @@ class Campaign(Base):
     obj_name = 'campaigns'
 
     def getId(self):
-        return self.get('campaignId')
+        return self.get('id')
 
     def getName(self):
         return self.get('name')
 
     def get_create_url(self):
-        return '{0}/video-management/v1/organizations/{1}/advertisers/{2}/campaigns'.format(Base.connection.url, self.get('organization_id'), self.get('advertiser_id'))
+        return '{0}/video-management/v2/organizations/{1}/advertisers/{2}/campaigns'.format(Base.connection.url, self.get('organization_id'), self.get('advertiser_id'))
 
     # get campaign by advertiser/org id
     def get_list_by_advertiser(self, advertiser_id, organization_id):
-        url = '{0}/video-management/v1/organizations/{1}/advertisers/{2}/campaigns'.format(Base.connection.url, organization_id, advertiser_id)
+        url = '{0}/video-management/v2/organizations/{1}/advertisers/{2}/campaigns'.format(Base.connection.url, organization_id, advertiser_id)
 
         method = "GET"
 
@@ -29,7 +29,7 @@ class Campaign(Base):
 
     # get campaigns by id
     def get_campaign_by_id(self, organization_id, advertiser_id, campaign_id):
-        url = '{0}/video-management/v1/organizations/{1}/advertisers/{2}/campaigns/{3}'.format(Base.connection.url, organization_id, advertiser_id, campaign_id)
+        url = '{0}/video-management/v2/organizations/{1}/advertisers/{2}/campaigns/{3}'.format(Base.connection.url, organization_id, advertiser_id, campaign_id)
         method = 'GET'
         response = self._execute(method, url, '')
 
--- tests/models/campaign_test.py
+++ tests/models/campaign_test.py
@@ -5,29 +5,21 @@ from aolclient.models.campaign import Campaign
 from aolclient.models.advertiser import Advertiser
 from tests.base import Base
 
-
 class CampaignTest(Base):
 
-    def testCreate(self):
-        campaign = Campaign(CampaignTest.conn)
-        campaign['advertiser_id'] = 25270
-        campaign['organization_id'] = 11357
-        campaign['name'] = "arun's campaign test"
-        campaign['startDate'] = "2014-10-17T00:00:00Z"
-        campaign['endDate'] = "2015-10-17T00:00:00Z"
-        campaign['status'] = "LIVE"
-        assert campaign.create() != None
+    def test_get_create_url(self):
+        campaign_instance = Campaign(CampaignTest.conn)
+        assert campaign_instance.get_create_url() == 'https://sandbox.video.advertising.aol.com/peach-rest-api/video-management/v2/organizations/None/advertisers/None/campaigns'
 
-    def testGetByCampaignId(self):
-        campaign = Campaign(CampaignTest.conn)
-        assert campaign.get_campaign_by_id('11357', '25270', '77527') == {u'status': u'LIVE', u'startDate': u'2014-10-17T00:00:00Z', u'endDate': u'2015-10-17T00:00:00Z', u'name': u"arun's campaign test", u'campaignId': 77527, u'organizationId': 11357, u'organizationSapId': 7000077134, u'currencyCode': None, u'advertiserId': 25270, u'customFieldValues': [], u'goals': [], u'advertiserSapId': 0}
-
-    def testGetByAdvertiser(self):
+    def test_get_list_by_advertiser(self):
         loader = Campaign(CampaignTest.conn)
         campaigns = loader.get_list_by_advertiser(25270, 11357)
-        assert len(campaigns) >= 1
+        campaign_ids = []
+        for campaign in campaigns:
+            campaign_ids.append(campaign.getId())
+        assert 77061 in campaign_ids
 
-    def test_get_id(self):
+    def test_get_campaign_by_id(self):
         campaign = Campaign(CampaignTest.conn)
-        campaignId = campaign.get_campaign_by_id('11357', '25270', '77061')['campaignId']
-        assert campaignId == 77527
+        campaign_info = campaign.get_campaign_by_id(11357, 25270, 77061)
+        assert campaign_info == {u'fcapAmount': 0, u'status': u'LIVE', u'conversionEvents': [], u'createdFromAuction': False, u'name': u'Hiscox American Courage Q3', u'fcapResetPeriod': 0, u'servingEndDate': u'2015-10-10T23:59:00-0700', u'organizationSapId': 7000077134, u'currencyCode': u'USD', u'apps': [{u'campaignAppStatus': u'READY', u'appName': u'ONE by AOL: Video Viewability Targeting and Optimization', u'logoUrl': u'http://cdn.adap.tv/rona/one-by-aol-04132015114756-175.png', u'feeInfo': u'Free', u'appId': 16}], u'brandId': 19390, u'flights': [{u'startDate': u'2015-09-08T00:00:00-0700', u'endDate': u'2015-10-10T23:59:00-0700', u'id': 127238}], u'brandName': u'American Courage', u'apertureEnabled': False, u'goals': [{u'goal': 57000.0, u'campaignId': 77061, u'deliveryCappingResetPeriod': u'TOTAL', u'ordering': 1, u'deliveryCappingType': u'GROSS_REVENUE', u'id': 31341}], u'advertiserSapId': 0, u'servingStartDate': u'2015-09-08T00:00:00-0700', u'id': 77061}
